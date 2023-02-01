65b12f80f66a3a0026745bb2fb8be527cc4b2645
mikaellaba <117746929+mikaellaba@users.noreply.github.com>
2023-02-01 18:46:35 +0800

    Add files via upload

---
+++ TUGASAKHIR1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.akhir.pkg1;
+
+/**
+ *
+ * @author ACER
+ */
+public class TUGASAKHIR1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("  *    *     *           * ");
+        System.out.println("  *    *     * *       * * ");
+        System.out.println("  *    *     *   *   *   * ");
+        System.out.println("  *    *     *     *     * ");
+        System.out.println("  *    *     *           * ");
+              
+    }
+}
+      
+        
+
+
+    
+
